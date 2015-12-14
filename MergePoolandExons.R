116b95aca2eb930b3ff381a23420f13affb21e2a
knkarthik <karthikuttan@gmail.com>
2015-12-14 15:23:40 +0530

    Delete MergePoolandExons.R

--- MergePoolandExons.R
+++
@@ -1,8 +0,0 @@
-for (i in 1:nrow(poolB)){
-  print(poolB[i,4])
-  amp = poolB[i,4]
-  if(amp %in% main[,4]){
-   # print("pool2")
-    main[main$Amplicon_ID==amp,5] = poolB[poolB$Amplicon_ID==amp,5]
-    }
-}
